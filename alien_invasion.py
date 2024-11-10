dc6e7bf5eacacfb818469a6b19b54569eaa19a0f
mfun.dev <personal.unknown.101@proton.me>
2024-11-10 12:40:20 -0500

    Draw the Ship to the Screen & fix the bg_color

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     '''Overall class to manage game assets and behavior'''
@@ -17,6 +18,9 @@ class AlienInvasion:
 
         pygame.display.set_caption('Alien Invasion')
 
+        # Create the ship
+        self.ship = Ship(self)
+
     def run_game(self):
         '''Start the main loop for the game'''
 
@@ -29,6 +33,7 @@ class AlienInvasion:
 
             #Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
