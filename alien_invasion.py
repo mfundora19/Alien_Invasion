d46c4cff7328aa967ba41c3a9f5890ea75b136be
mfun.dev <personal.unknown.101@proton.me>
2024-11-14 22:46:09 -0500

    Creating the fleet, 1st row of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -101,9 +101,20 @@ class AlienInvasion:
     def _create_fleet(self):
         '''Create the fleet of aliens'''
 
-        #Make alien
+        # Create an alien and keep adding aliens until there's no room left
+        # Spacing b\w aliens is one alien width
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2*alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
+
+            self.aliens.add(alien)
 
     def _update_screen(self):
         '''Update images on the screen, and flip the new screen.'''
